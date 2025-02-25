0e4abb37439d487ccb82fb958a8c6800b080f4f4
URSA0112 <Nimone99@gmail.com>
2025-02-25 10:37:40 +0800

    snake failnii

--- src/app/page.js
+++ src/app/page.js
@@ -3,14 +3,42 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   const [position, setPosition] = useState({ x: 0, y: 0 })
+  const [direction, setDirection] = useState("RIGHT")
   const unit = 50;
 
- 
+  useEffect(() => {
+    const HandleKeyBoardMovement = (e) => {
+      switch (e.key) {
+        case "ArrowUp": setDirection("UP"); console.log("up")
+          break;
+        case "ArrowDown": setDirection("DOWN"); console.log("down")
+          break;
+        case "ArrowLeft": setDirection("LEFT"); console.log("left")
+          break;
+        case "ArrowRight": setDirection("RIGHT"); console.log("right")
+          break;
+          default:
+          break;
 
-  setInterval(() => {
-    position.x++
+      }
+    };
+    window.addEventListener("keydown", HandleKeyBoardMovement);
+    return () => window.removeEventListener("keydown", HandleKeyBoardMovement)
 
-  }, 2000);
+  }, [])
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setPosition((prev) => {
+        if (direction === "RIGHT") { return { ...prev, x: prev.x + unit  } }
+        if (direction === "LEFT") { return { ...prev, x: prev.x - unit } }
+        if (direction === "DOWN") { return { ...prev, y: prev.y + unit } }
+        if (direction === "UP") { return {  ...prev,y: prev.y - unit } };
+        return prev;
+      })
+    }, 1000)
+
+  }, [direction])
+
 
 
 
@@ -18,10 +46,8 @@ export default function Home() {
     <div id="gaming-board" className="bg-white w-[500px] h-[500px] 
     justify-self-center mt-[50%] translate-y-[-50%] relative ">
       <div id="snake" className="bg-pink-600 w-[50px] h-[50px] absolute"
-        style={{ left: position.x, top: position.y }} ></div>
-
-
-
+        style={{ left: position.x, top: position.y }}
+      ></div>
 
     </div>
   );
