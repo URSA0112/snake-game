8517ea3d3d0525d1add72d6baa3f059149afece2
URSA0112 <Nimone99@gmail.com>
2025-02-27 09:48:45 +0800

    save

--- src/app/page.js
+++ src/app/page.js
@@ -3,41 +3,40 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   const [position, setPosition] = useState({ x: 0, y: 0 })
-  const [direction, setDirection] = useState("RIGHT")
+  const [direction, setDirection] = useState("")
   const unit = 50;
 
   useEffect(() => {
     const HandleKeyBoardMovement = (e) => {
       switch (e.key) {
-        case "ArrowUp": setDirection("UP"); console.log("up")
+        case "ArrowUp": setDirection("UP"); console.log(direction)
           break;
-        case "ArrowDown": setDirection("DOWN"); console.log("down")
+        case "ArrowDown": setDirection("DOWN"); console.log(direction)
           break;
-        case "ArrowLeft": setDirection("LEFT"); console.log("left")
+        case "ArrowLeft": setDirection("LEFT"); console.log(direction)
           break;
-        case "ArrowRight": setDirection("RIGHT"); console.log("right")
+        case "ArrowRight": setDirection("RIGHT"); console.log(direction)
           break;
-          default:
+        default:
           break;
-
       }
     };
     window.addEventListener("keydown", HandleKeyBoardMovement);
     return () => window.removeEventListener("keydown", HandleKeyBoardMovement)
-
   }, [])
+
   useEffect(() => {
     const interval = setInterval(() => {
       setPosition((prev) => {
-        if (direction === "RIGHT") { return { ...prev, x: prev.x + unit  } }
-        if (direction === "LEFT") { return { ...prev, x: prev.x - unit } }
-        if (direction === "DOWN") { return { ...prev, y: prev.y + unit } }
-        if (direction === "UP") { return {  ...prev,y: prev.y - unit } };
+        if (direction === "RIGHT") return { x: prev.x + unit, y: prev.y }
+        if (direction === "LEFT") return { x: prev.x - unit, y: prev.y }
+        if (direction === "DOWN") return { y: prev.y + unit, x: prev.x }
+        if (direction === "UP") return { y: prev.y - unit, x: prev.x }
         return prev;
-      })
-    }, 1000)
-
-  }, [direction])
+      });
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [direction,position])
 
 
 
